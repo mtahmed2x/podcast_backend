498924d89b1cc7ba692a03b5e5af70b0f80a6196
Mir Tanim Ahmed <mtahmed_dev@proton.me>
2025-01-11 18:51:13 +0600

    podcast fix

--- src/routers/podcast.ts
+++ src/routers/podcast.ts
@@ -7,14 +7,15 @@ import fileHandler from "@middlewares/fileHandler";
 
 const router = express.Router();
 
+router.get("/popular", authorize, PodcastServices.popularPodcasts);
+router.get("/latest", authorize, PodcastServices.latestPodcasts);
+
 router.post("/create", fileUpload(), fileHandler, authorize, PodcastController.create);
 router.get("/", authorize, PodcastController.getAll);
 router.get("/:id", authorize, PodcastController.get);
 router.put("/update/:id", fileUpload(), fileHandler, authorize, PodcastController.update);
 router.delete("/delete/:id", authorize, PodcastController.remove);
 
-router.get("/popular", authorize, PodcastServices.popularPodcasts);
-router.get("/latest", authorize, PodcastServices.latestPodcasts);
 router.post("/play/:id", authorize, PodcastServices.play);
 router.post("/play-next/:id", authorize, PodcastServices.playNext);
 router.post("/report", authorize, PodcastServices.reportPodcast);
